d5214e142ffd239b60cd955fdf68cf991db0897e
Pianissimo7 <edanphi@gmail.com>
2021-11-28 19:58:51 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# import socket module
+from socket import *
+import sys  # In order to terminate the program
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Prepare a sever socket
+HOST = "172.19.128.1"
+PORT = 6789
+
+serverSocket.bind((HOST, PORT))
+serverSocket.listen(1)
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        # outputdata = connectionSocket.recv(1024)
+        # Send one HTTP header line into socket
+        outputdata = []
+        outputdata.append('HTTP/1.0 200 OK\n\n')
+        outputdata.append(f.read())
+        # connectionSocket.sendall(outputdata.encode())
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
+    except IOError as err:
+        print("file not found: ", err)
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
